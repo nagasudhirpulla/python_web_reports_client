9a9edf8c5b4d10aee55400ab4b475ce4fba12baa
nagasudhirpulla <nagasudhirpulla@gmail.com>
2018-06-27 23:53:48 +0530

    psp stats excel ui poc done

--- app/ui_modules/excel_ui.py
+++ app/ui_modules/excel_ui.py
@@ -17,6 +17,7 @@ import datetime as dt
 
 import os
 
+import numpy as np
 # get the directory of the script file
 # print(os.path.dirname(os.path.realpath(__file__)))
 inputFileFolder = r'C:\Users\Nagasudhir\Documents\Python Projects\python_web_reports_cient\app\ui_modules'
@@ -47,8 +48,18 @@ writer.save()
 # result = pd.merge(left, right, left_on='key', right_index=True, how='left', sort=False);
 leftTable = analysisKeyValuesInputDF.groupby(['entity', 'key']).first()
 resultDF = pd.merge(leftTable, statsDF, left_index=True, right_index=True, how='left', sort=False);
-# todo create min_violated, max_violated, mean_deviation columns
+
+# create min_violated, max_violated, violated, is_Blank, mean_deviation_percentage columns
+val_col = pd.to_numeric(resultDF['value'], errors='coerce')
+resultDF['min_violated'] = np.where((pd.notnull(val_col))&(val_col<resultDF['min_cap']), 1, 0)
+resultDF['max_violated'] = np.where((pd.notnull(val_col))&(val_col>resultDF['max_cap']), 1, 0)
+resultDF['violated'] = resultDF['min_violated'] | resultDF['max_violated']
+resultDF['is_Blank'] = np.where((~pd.notnull(val_col)), 1, 0)
+resultDF['prev_band_violated'] = np.where((pd.notnull(val_col))&(val_col<resultDF['min']), 1, 0) | np.where((pd.notnull(val_col))&(val_col>resultDF['max']), 1, 0)
+resultDF['mean_deviation_perc'] = np.where(resultDF['is_Blank'] == 0, 100*(val_col-resultDF['mean'])/resultDF['mean'], None)
+
 # todo re-arrange columns
+resultDF = resultDF[['value', 'min_cap', 'max_cap', 'mean', 'mean_deviation_perc', 'violated', 'prev_band_violated', 'min', 'max', 'is_Blank', 'min_violated', 'max_violated', 'time']]
 
 # dump the results file
 outFilename = 'results_'+dt.datetime.now().strftime('%d_%m_%y_%H_%M_%S')+'.xlsx'
